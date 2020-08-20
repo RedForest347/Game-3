f45905a96e915e14c9d5f84f128b97b0e1b8bb9a
Red <69144172+RedForest347@github.com>
2020-08-20 15:01:39 +0300

    Begin some mechanics
    Начата механика управления камерой, движения

---
+++ Assets/TestF/PlayerController/Data/MoveControls.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MoveControls", menuName = "Subcomponents/MoveControls", order = 51)]
+public class MoveControls : ScriptableObject
+{
+    public KeyCode Forvard;
+    public KeyCode Backward;
+    public KeyCode Left;
+    public KeyCode Rigt;
+}
